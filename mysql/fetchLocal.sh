ca202da41a9f6ebd04849982d950ba0d3ea0dc06
KinoThe-Kafkaesque <abdellahkadem5@gmail.com>
2023-02-06 22:08:38 +0100

    backuplocal

---
+++ mysql/fetchLocal.sh
@@ -0,0 +1,9 @@
+# Load environment variables from .env file
+#!/bin/bash
+source .env
+
+# Connect to the LOCAL MySQL server
+# mysql -h $MYSQL_LOCAL_HOST -u $MYSQL_LOCAL_USERNAME -p$MYSQL_LOCAL_PASSWORD
+
+# Dump the LOCAL database to a file
+mysqldump -h $MYSQL_LOCAL_HOST -P $MYSQL_LOCAL_PORT -u $MYSQL_LOCAL_USERNAME -p$MYSQL_LOCAL_PASSWORD $MYSQL_LOCAL_DATABASE --single-transaction | pv > database_backup_local.sql
