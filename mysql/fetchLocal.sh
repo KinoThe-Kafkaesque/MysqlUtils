5ec724ae0c9ac09de2fb7fa4fdcc79f9c5d691ed
KinoThe-Kafkaesque <abdellahkadem5@gmail.com>
2023-02-23 14:27:49 +0100

    ignore gid

--- mysql/fetchLocal.sh
+++ mysql/fetchLocal.sh
@@ -6,4 +6,4 @@ source .env
 # mysql -h $MYSQL_LOCAL_HOST -u $MYSQL_LOCAL_USERNAME -p$MYSQL_LOCAL_PASSWORD
 
 # Dump the LOCAL database to a file
-mysqldump -h $MYSQL_LOCAL_HOST -P $MYSQL_LOCAL_PORT -u $MYSQL_LOCAL_USERNAME -p$MYSQL_LOCAL_PASSWORD $MYSQL_LOCAL_DATABASE --single-transaction | pv > database_backup_local.sql
+mysqldump -h $MYSQL_LOCAL_HOST -P $MYSQL_LOCAL_PORT -u $MYSQL_LOCAL_USERNAME -p$MYSQL_LOCAL_PASSWORD $MYSQL_LOCAL_DATABASE --single-transaction --set-gtid-purged=OFF | pv > database_backup_local.sql
--- mysql/fetchRemote.sh
+++ mysql/fetchRemote.sh
@@ -6,4 +6,4 @@ source .env
 # mysql -h $MYSQL_REMOTE_HOST -u $MYSQL_REMOTE_USERNAME -p$MYSQL_REMOTE_PASSWORD
 
 # Dump the remote database to a file
-mysqldump -h $MYSQL_REMOTE_HOST -P $MYSQL_REMOTE_PORT -u $MYSQL_REMOTE_USERNAME -p$MYSQL_REMOTE_PASSWORD $MYSQL_REMOTE_DATABASE --single-transaction | pv > database_backup.sql
+mysqldump -h $MYSQL_REMOTE_HOST -P $MYSQL_REMOTE_PORT -u $MYSQL_REMOTE_USERNAME -p$MYSQL_REMOTE_PASSWORD $MYSQL_REMOTE_DATABASE --single-transaction  --set-gtid-purged=OFF | pv > database_backup.sql
